378530cf7b9f409fa67a9d332a54411da3ed1211
kabra-palak <kabrapalak08@gmail.com>
2026-08-11 02:14:09 +0530

    feat: add Prisma setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
@@ -8,6 +9,7 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json()); 
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
   res.send('Welcome to the server!');
