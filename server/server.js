bbc6c2b3df3212371445e9967c15e18665a2a0dd
kabra-palak <kabrapalak08@gmail.com>
2026-08-12 23:20:30 +0530

    feat: add ingest functions and vercel setup

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { clerkMiddleware } from '@clerk/express'
-
+import serve from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 const app = express();
 
 dotenv.config();
@@ -11,9 +12,10 @@ app.use(cors());
 app.use(express.json()); 
 app.use(clerkMiddleware())
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the server!');
-});
+app.get('/', (req, res) => res.send('Welcome to the server!'));
+
+app.use("/api/inngest", serve({client: inngest, functions}));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
